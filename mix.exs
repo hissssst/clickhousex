c02705d18274b13bd71487bd92bc73a77d6160df
Simon Žlender <simon.zlender@gmail.com>
2019-09-25 16:23:34 +0200

    Update db_connection

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Clickhousex.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:db_connection, "~> 2.0.0"},
+      {:db_connection, "~> 2.1.0"},
       {:httpoison, "~> 1.5"},
       {:jason, "~> 1.1.2"},
       {:ex_doc, "~> 0.19", only: :dev},
